533b7b2a2855a16708401845d97227058d040a2a
dneklyudov <dneklyudov@i-novus.ru>
2023-04-18 22:33:54 +0300

    storage service - fix

--- constants/messages.js
+++ constants/messages.js
@@ -2,4 +2,5 @@ export const messages = {
   SET_TOKEN_SUCCESS : 'Token is successfully saved',
   SET_CITY_SUCCESS : 'New city is successfully saved',
   SET_VALUE_FAILURE: 'Something went wrong.',
+  EMPTY_VALUE: 'Value can not be empty.'
 }
--- helpers/cliMethods.js
+++ helpers/cliMethods.js
@@ -2,6 +2,12 @@ import { LogService, StorageService } from "../services/index.js";
 import { dataKeyNames, messages } from "../constants/index.js";
 
 export const setToken = async token => {
+  if (typeof token !== 'string' || !token.length) {
+    LogService.logError(messages.EMPTY_VALUE)
+
+    return
+  }
+
   try {
     const isSaved = await StorageService.saveValue({ key: dataKeyNames.TOKEN, value: token })
 
