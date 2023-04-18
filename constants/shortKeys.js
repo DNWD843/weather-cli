1902e44aa86fbf7c73c9956871c623d46e4a2799
dneklyudov <dneklyudov@i-novus.ru>
2023-04-19 00:02:18 +0300

    fixed help, extended api

--- constants/shortKeys.js
+++ constants/shortKeys.js
@@ -2,6 +2,8 @@ export const shortKeys = {
   HELP: 'h',
   CITY: 'c',
   TOKEN: 't',
+  QUERY_PARAMS: 'p',
+  SHOW_SAVED_CITY: 's',
 }
 
 export const dataKeyNames = {
--- services/log.service.js
+++ services/log.service.js
@@ -2,17 +2,32 @@ import chalk from 'chalk'
 import dedent from 'dedent-js'
 import { shortKeys, titles } from "../constants/index.js";
 
-const { bgRed, bgGreen, red  } = chalk
+const { bgRed, bgGreen, red, magenta  } = chalk
 
 const lightGrey = chalk.hex('#DCDCDC')
 const gold = chalk.hex('#FFD700')
 
 export class LogService {
   static #help = dedent`${chalk.bgHex('#FFD700').hex('#D2691E').bold(` ${titles.HELP} `)}
-    ${gold('no-parameters')} ${lightGrey('- to see forecast for saved city')}
-    ${gold(`-${shortKeys.CITY}`)}  ${lightGrey('[CITY] to set city')}
-    ${gold(`-${shortKeys.TOKEN}`)}  ${lightGrey('[API_KEY] to set token')}
-    ${gold(`-${shortKeys.HELP}`)}  ${lightGrey('[HELP] to see help')}
+    ${gold('no-parameters')} ${lightGrey('- to get forecast by saved city')}
+    ${gold(`-${shortKeys.QUERY_PARAMS} [CITY_NAME]`)} ${lightGrey('- to get forecast by passed city name')}
+    ${gold(`-${shortKeys.QUERY_PARAMS} [CITY_NAME COUNTRY_CODE]`)} ${lightGrey('- to get forecast by passed city name and country code')}
+    ${gold(`-${shortKeys.QUERY_PARAMS} [CITY_NAME STATE_CODE COUNTRY_CODE]`)} ${lightGrey('- to get forecast by passed city name, state code and country code')}
+    ${gold(`-${shortKeys.CITY} [CITY_NAME] or [CITY_NAME/COUNTRY_CODE] or [CITY_NAME/STATE_CODE/COUNTRY_CODE]`)}  ${lightGrey('- to set city')}
+    ${gold(`-${shortKeys.TOKEN} [API_KEY]`)}  ${lightGrey('- to set token')}
+    ${gold(`-${shortKeys.SHOW_SAVED_CITY}`)}  ${lightGrey('- to see saved city')}
+    ${gold(`-${shortKeys.HELP}`)}  ${lightGrey('- to see help')}
+    
+    ${magenta('EXAMPLES:')}
+    ${magenta('weather')}
+    ${magenta('weather -p saint-petersburg')}
+    ${magenta('weather -p saint-petersburg us')}
+    ${magenta('weather -p saint-petersburg colorado us')}
+    ${magenta('weather -c moscow')}
+    ${magenta('weather -c moscow/us')}
+    ${magenta('weather -c moscow/idaho/us')}
+    ${magenta('weather -t some_api_key')}
+    ${magenta('weather -s')}
   `
 
   static logError = err => {
