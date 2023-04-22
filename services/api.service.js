d9c14b9c94f4b17f5c9ac6cfb6b8b5f6f76df6ab
dneklyudov <dneklyudov@i-novus.ru>
2023-04-22 21:01:57 +0300

    added weather request based on https

--- services/api.service.js
+++ services/api.service.js
@@ -1,7 +1,33 @@
-import axios from "axios";
-import { apiConfig, GEO_CODING_URL, WEATHER_URL } from "../constants/index.js";
+import axios from 'axios'
+import { apiConfig, GEO_CODING_URL, WEATHER_URL } from '../constants/index.js'
+import https from 'https'
 
 export class ApiService {
+  /**
+   * @deprecated
+   * Просто демонстрация реализации на https
+   */
+  static async #getWeather_DEPRECATED({lat, lon, token}) {
+    return new Promise((resolve, reject) => {
+      const url = new URL(WEATHER_URL)
+      url.searchParams.append('lat', lat)
+      url.searchParams.append('lon', lon)
+      url.searchParams.append('appid', token)
+      url.searchParams.append('lang', 'ru')
+      url.searchParams.append('units', 'metric')
+      url.searchParams.append('limit', '1')
+
+      https.get(url, response => {
+        let res = ''
+        response.on('data', chunk => {
+          res += chunk
+        })
+
+        response.on('end', () => resolve(res))
+        response.on('error', error => reject(error))
+      })
+    })
+  }
   static async fetchCoordinates({ cityParams, token }) {
     const { data } = await axios.get(GEO_CODING_URL, {
       params: {
